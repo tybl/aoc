3969ca21a3fa9501314550ed5d415d013c141dc3
Brandon Lyons <t.brandon.lyons@gmail.com>
2026-01-18 11:05:08 -0500

    Day 1, problem 1

--- 2025/day1/problem_1/src/main.rs
+++ 2025/day1/problem_1/src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+use std::fs;
+
+/// Command line parameters
+#[derive(Parser)]
+pub struct CliArguments {
+    #[arg(short, long, default_value = "input/0.txt")]
+    pub input: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut password = 0;
+    let mut dial = 50;
+    let args = CliArguments::parse();
+    let input = fs::read_to_string(args.input).unwrap();
+    let input = input.split("\n");
+    for line in input {
+        let mut line = line.chars();
+        match line.next() {
+            Some('L') => {
+                let num = line.collect::<String>().parse::<i32>().unwrap();
+                dial = (100 + dial - num) % 100;
+            }
+            Some('R') => {
+                let num = line.collect::<String>().parse::<i32>().unwrap();
+                dial = (dial + num) % 100;
+            }
+            Some(_) => panic!("Unknown instruction"),
+            None => {}
+        }
+        if 0 == dial {
+            password += 1;
+        }
+    }
+    println!("{password}");
 }
