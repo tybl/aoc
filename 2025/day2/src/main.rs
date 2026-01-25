be1cfccdfb26e7472fa8e68d0b16f3b1df64f11b
Brandon Lyons <t.brandon.lyons@gmail.com>
2026-01-25 10:59:52 -0500

    Day 2, problem 1

---
+++ 2025/day2/src/main.rs
@@ -0,0 +1,62 @@
+use clap::Parser;
+use std::fs;
+use std::ops::RangeInclusive;
+
+/// Command line parameters
+#[derive(Parser)]
+pub struct CliArguments {
+    #[arg(short, long, default_value = "input/0.txt")]
+    pub input: String,
+}
+
+fn to_range(input: &str) -> std::ops::RangeInclusive<i64> {
+    let extent: Vec<i64> = input
+        .split('-')
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
+    RangeInclusive::new(*extent.first().unwrap(), *extent.last().unwrap())
+}
+
+fn is_invalid(x: i64) -> bool {
+    let mut s = x.to_string();
+    let len = s.len();
+    if len % 2 == 0 {
+        let t = s.split_off(len / 2);
+        s == t
+    } else {
+        false
+    }
+}
+
+fn main() {
+    let args = CliArguments::parse();
+    let input = fs::read_to_string(args.input).unwrap();
+    let sum = input
+        .trim()
+        .split(',')
+        .map(|i| to_range(i))
+        .flatten()
+        .filter(|x| is_invalid(*x))
+        .sum::<i64>();
+    println!("{sum}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn convert_single_range() {
+        let input = "11-22";
+        let output = RangeInclusive::new(11, 22);
+        assert!(output == to_range(input));
+    }
+
+    #[test]
+    fn find_invalid_ids() {
+        assert!(is_invalid(22));
+        assert!(!is_invalid(23));
+        assert!(!is_invalid(233));
+        assert!(is_invalid(2323));
+    }
+}
