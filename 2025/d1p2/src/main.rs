c08accfed68a387f6f661b612f44beb81eb9287b
Brandon Lyons <tlyons@srcinc.com>
2026-01-28 08:08:39 -0500

    2025 Day 1, part 2: Brute forced

--- 2025/d1p2/src/main.rs
+++ 2025/d1p2/src/main.rs
@@ -8,42 +8,102 @@ pub struct CliArguments {
     pub input: String,
 }
 
-fn main() {
-    let mut password = 0;
-    let mut dial = 50;
-    println!("The dial starts by pointing at {dial}");
-    let args = CliArguments::parse();
-    let input = fs::read_to_string(args.input).unwrap();
-    let input = input.split("\n");
+const DIAL_MAX: i32 = 100;
+const INIT_DIAL: i32 = 50;
+
+fn count_zeros(init: i32, input: &str) -> u32 {
+    let mut count = 0;
+    let mut dial = init;
+    let input = input.lines();
     for line in input {
         let mut line = line.chars();
         match line.next() {
             Some('L') => {
+                let mut temp = dial;
                 let num = line.collect::<String>().parse::<i32>().unwrap();
-                let mut notice = String::from(".");
-                if dial - num < 0 {
-                    password += 1;
-                    notice = String::from("; during this rotation, it points at 0");
+                for _ in 0..num {
+                    temp = (temp - 1).rem_euclid(DIAL_MAX);
+                    if 0 == temp {
+                        count += 1;
+                    }
                 }
-                dial = (100 + dial - num) % 100;
-                println!("The dial is rotated L{num} to point at {dial}{notice}");
+                println!("{dial:3} - {num:2} -> {temp:3}");
+                dial = temp;
             }
             Some('R') => {
                 let num = line.collect::<String>().parse::<i32>().unwrap();
-                let mut notice = String::from(".");
-                if dial + num > 100 {
-                    password += 1;
-                    notice = String::from("; during this rotation, it points at 0");
-                }
-                dial = (dial + num) % 100;
-                println!("The dial is rotated R{num} to point at {dial}{notice}");
+                let temp = dial + num;
+                let rem = temp.rem_euclid(DIAL_MAX);
+                let div = temp.div_euclid(DIAL_MAX);
+                println!("{dial:3} + {num:2} -> {temp:3} -> {div:2} * {DIAL_MAX} + {rem:2}");
+                dial = rem;
+                count += div.abs_diff(0);
             }
             Some(_) => panic!("Unknown instruction"),
             None => {}
         }
-        //if 0 == dial {
-        //    password += 1;
-        //}
     }
+    count
+}
+
+fn main() {
+    let args = CliArguments::parse();
+    let input = fs::read_to_string(args.input).unwrap();
+    let password = count_zeros(INIT_DIAL, &input);
     println!("{password}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_step_01() {
+        assert!(1 == count_zeros(INIT_DIAL, "L68"));
+    }
+
+    #[test]
+    fn example_step_02() {
+        assert!(0 == count_zeros(82, "L30"));
+    }
+
+    #[test]
+    fn example_step_03() {
+        assert!(1 == count_zeros(52, "R48"));
+    }
+
+    #[test]
+    fn example_step_04() {
+        assert!(0 == count_zeros(0, "L5")); //
+    }
+
+    #[test]
+    fn example_step_05() {
+        assert!(1 == count_zeros(95, "R60"));
+    }
+
+    #[test]
+    fn example_step_06() {
+        assert!(1 == count_zeros(55, "L55")); //
+    }
+
+    #[test]
+    fn rotr50() {
+        assert!(1 == count_zeros(INIT_DIAL, "R50"));
+    }
+
+    #[test]
+    fn rotl50() {
+        assert!(1 == count_zeros(INIT_DIAL, "L50"));
+    }
+
+    #[test]
+    fn rotl50l100() {
+        assert!(2 == count_zeros(INIT_DIAL, "L50\nL100"));
+    }
+
+    #[test]
+    fn rotl250() {
+        assert!(3 == count_zeros(INIT_DIAL, "L250"));
+    }
 }
