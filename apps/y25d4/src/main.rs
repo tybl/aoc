5914734083a8396f976a79fc5c3c91304320ac0f
Brandon Lyons <t.brandon.lyons@gmail.com>
2026-02-08 16:28:07 -0500

    2025 day 4, part 2

--- apps/y25d4/src/main.rs
+++ apps/y25d4/src/main.rs
@@ -2,58 +2,84 @@ use clap::Parser;
 use helpers::CliArguments;
 use std::fs;
 
+fn remove_rolls(input: &mut Vec<Vec<u8>>) -> i32 {
+    let mut removed_count = 0;
+    let height = input.len();
+    for row in 0..height {
+        let width = input[row].len();
+        for col in 0..width {
+            let prev_row = row.checked_sub(1);
+            let prev_col = col.checked_sub(1);
+            let next_row = row + 1;
+            let next_col = col + 1;
+            if input[row][col] == '@' as u8 {
+                let mut surround_count = 0;
+                if let Some(prev_row) = prev_row {
+                    if let Some(prev_col) = prev_col {
+                        if input[prev_row][prev_col] != '.' as u8 {
+                            surround_count += 1;
+                        }
+                    }
+                    if input[prev_row][col] != '.' as u8 {
+                        surround_count += 1;
+                    }
+                    if next_col < width {
+                        if input[prev_row][next_col] != '.' as u8 {
+                            surround_count += 1;
+                        }
+                    }
+                }
+                if let Some(prev_col) = prev_col {
+                    if input[row][prev_col] != '.' as u8 {
+                        surround_count += 1;
+                    }
+                }
+                if next_col < width {
+                    if input[row][next_col] != '.' as u8 {
+                        surround_count += 1;
+                    }
+                }
+                if next_row < height {
+                    if let Some(prev_col) = prev_col {
+                        if input[next_row][prev_col] != '.' as u8 {
+                            surround_count += 1;
+                        }
+                    }
+                    if input[next_row][col] != '.' as u8 {
+                        surround_count += 1;
+                    }
+                    if next_col < width {
+                        if input[next_row][next_col] != '.' as u8 {
+                            surround_count += 1;
+                        }
+                    }
+                }
+                if surround_count < 4 {
+                    input[row][col] = '*' as u8;
+                    removed_count += 1;
+                }
+            }
+        }
+    }
+    for row in input {
+        for ch in row {
+            *ch = if *ch == '*' as u8 { '.' as u8 } else { *ch }
+        }
+    }
+    removed_count
+}
+
 fn main() {
     let args = CliArguments::parse();
     let input = fs::read_to_string(args.input).unwrap();
-    let width = input.find('\n').unwrap();
-    let height = input.lines().count();
-    println!("width: {width}, height: {height}");
-    let mut result = vec![vec![0; width]; height];
-    let mut row: i32 = 0;
-    let mut col: i32 = 0;
-    for ch in input.chars() {
-        if ch == '\n' {
-            row += 1;
-            col = 0;
-            continue;
-        }
-        let prev_row = row - 1;
-        let prev_col = col - 1;
-        let next_row = row + 1;
-        let next_col = col + 1;
-        if ch == '@' {
-            if 0 <= prev_col {
-                if 0 <= prev_row {
-                    result[prev_row as usize][prev_col as usize] += 1;
-                }
-                result[row as usize][prev_col as usize] += 1;
-                if next_row < height as i32 {
-                    result[next_row as usize][prev_col as usize] += 1;
-                }
-            }
-            if 0 <= prev_row {
-                result[prev_row as usize][col as usize] += 1;
-            }
-            //result[row as usize][col as usize] += 1;
-            if next_row < height as i32 {
-                result[next_row as usize][col as usize] += 1;
-            }
-            if next_col < width as i32 {
-                if 0 <= prev_row {
-                    result[prev_row as usize][next_col as usize] += 1;
-                }
-                result[row as usize][next_col as usize] += 1;
-                if next_row < height as i32 {
-                    result[next_row as usize][next_col as usize] += 1;
-                }
-            }
-        }
-        if ch == '.' {
-            // Nothing to grab here, remove from consideration
-            result[row as usize][col as usize] += 10;
-        }
-        col += 1;
+    let mut input: Vec<Vec<u8>> = input.lines().map(|l| l.bytes().collect()).collect();
+    let part1 = remove_rolls(&mut input);
+    let mut part2 = 0;
+    let mut step = part1;
+    while step != 0 {
+        part2 += step;
+        step = remove_rolls(&mut input);
     }
-    let answer = result.iter().flatten().filter(|&x| *x < 4).count();
-    println!("{answer}");
+    println!("Part1: {part1}");
+    println!("Part2: {part2}");
 }
