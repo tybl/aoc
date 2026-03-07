e23ebe67bdde43fdc6328deae25b75324285794a
Brandon Lyons <t.brandon.lyons@gmail.com>
2026-03-07 15:58:34 -0500

    Part 1

--- apps/y25d6/src/main.rs
+++ apps/y25d6/src/main.rs
@@ -1,3 +1,58 @@
+use clap::Parser;
+use helpers::CliArguments;
+use std::fs;
+
+/// Part 1
+
+#[derive(Debug)]
+enum Ops {
+    Add(u64),
+    Mul(u64),
+}
+
+fn part_one(input: &str) -> u64 {
+    println!("here");
+    let mut lines = input
+        .lines()
+        .map(|l| l.split(' ').filter(|w| !w.is_empty()))
+        .rev();
+    let mut accumulators = lines
+        .next()
+        .unwrap()
+        .map(|w| {
+            if w == "+" {
+                Ops::Add(0)
+            } else if w == "*" {
+                Ops::Mul(1)
+            } else {
+                panic!("Invalid operator: {w}")
+            }
+        })
+        .collect::<Vec<Ops>>();
+    for line in lines {
+        for (acc, n) in std::iter::zip(&mut accumulators, line) {
+            let n = n.parse::<u64>().unwrap();
+            match acc {
+                Ops::Add(sum) => *sum += n,
+                Ops::Mul(prod) => *prod *= n,
+            }
+        }
+    }
+    accumulators.iter().fold(0, |acc, x| match x {
+        Ops::Add(x) => acc + x,
+        Ops::Mul(x) => acc + x,
+    })
+}
+
+/// Part 2
+
+fn part_two(_input: &str) -> i64 {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArguments::parse();
+    let input = fs::read_to_string(args.input).unwrap();
+    println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 }
