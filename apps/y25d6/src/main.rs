3f6de4e7a141520dba9449ec18627e72d896f608
Brandon Lyons <t.brandon.lyons@gmail.com>
2026-03-08 13:25:34 -0400

    Part 2

--- apps/y25d6/src/main.rs
+++ apps/y25d6/src/main.rs
@@ -8,10 +8,10 @@ use std::fs;
 enum Ops {
     Add(u64),
     Mul(u64),
+    Invalid,
 }
 
 fn part_one(input: &str) -> u64 {
-    println!("here");
     let mut lines = input
         .lines()
         .map(|l| l.split(' ').filter(|w| !w.is_empty()))
@@ -35,19 +35,64 @@ fn part_one(input: &str) -> u64 {
             match acc {
                 Ops::Add(sum) => *sum += n,
                 Ops::Mul(prod) => *prod *= n,
+                Ops::Invalid => {}
             }
         }
     }
     accumulators.iter().fold(0, |acc, x| match x {
         Ops::Add(x) => acc + x,
         Ops::Mul(x) => acc + x,
+        Ops::Invalid => panic!("Should not reach this point"),
     })
 }
 
 /// Part 2
 
-fn part_two(_input: &str) -> i64 {
-    0
+fn part_two(input: &str) -> u64 {
+    let input = input.lines().map(|l| l.as_bytes()).collect::<Vec<&[u8]>>();
+    let len = input.first().unwrap().len();
+    for row in &input {
+        assert_eq!(len, row.len());
+    }
+    let mut acc = Ops::Invalid;
+    let mut total = 0;
+    for i in 0..len {
+        if input.iter().all(|row| row[i] == ' ' as u8) {
+            match acc {
+                Ops::Invalid => {}
+                Ops::Add(n) => total += n,
+                Ops::Mul(n) => total += n,
+            }
+            acc = Ops::Invalid;
+        } else {
+            // Do calc
+            let mut num = 0;
+            for row in &input {
+                let c = row[i] as char;
+                match c {
+                    // Assuming operators are specified in the last row
+                    '*' => acc = Ops::Mul(1),
+                    '+' => acc = Ops::Add(0),
+                    ' ' => {} // Assuming whitespaces can be ignored
+                    _ if c.is_digit(10) => {
+                        num = num * 10 + c.to_digit(10).unwrap() as u64;
+                    }
+                    _ => panic!("Unexpected character!"),
+                }
+            }
+            match &mut acc {
+                Ops::Invalid => panic!("That was unexpected!"),
+                Ops::Add(sum) => *sum += num,
+                Ops::Mul(product) => *product *= num,
+            }
+        }
+    }
+    match acc {
+        Ops::Invalid => {}
+        Ops::Add(n) => total += n,
+        Ops::Mul(n) => total += n,
+    }
+    total
 }
 
 fn main() {
