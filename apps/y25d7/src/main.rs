ccf2cee253f8c52304bcf52a03d4a18953db502e
Brandon Lyons <t.brandon.lyons@gmail.com>
2026-03-15 12:04:10 -0400

    Part 2

--- apps/y25d7/src/main.rs
+++ apps/y25d7/src/main.rs
@@ -1,11 +1,13 @@
 use clap::Parser;
 use helpers::CliArguments;
+use std::collections::{HashMap, hash_map::Entry};
 use std::fs;
 
 fn main() {
     let args = CliArguments::parse();
     let input = fs::read_to_string(args.input).unwrap();
     println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 }
 
 fn part_one(input: &str) -> usize {
@@ -34,11 +36,45 @@ fn part_one(input: &str) -> usize {
             }
         }
     }
-    for r in input {
-        for c in r {
-            print!("{}", c as char);
-        }
-        println!("");
-    }
     count
+}
+
+fn part_two(input: &str) -> usize {
+    let input: Vec<Vec<u8>> = input.lines().map(|l| l.bytes().collect()).collect();
+    let mut memo = HashMap::new();
+    let start_col = input
+        .first()
+        .unwrap()
+        .iter()
+        .position(|c| *c == 'S' as u8)
+        .unwrap();
+    dfs(&input, 1, start_col, &mut memo)
+}
+
+fn dfs(
+    tree: &Vec<Vec<u8>>,
+    row: usize,
+    col: usize,
+    mut memo: &mut HashMap<(usize, usize), usize>,
+) -> usize {
+    if row < tree.len() {
+        match memo.entry((row, col)) {
+            Entry::Occupied(o) => *o.get(),
+            Entry::Vacant(_) => {
+                let count = match tree[row][col] as char {
+                    '^' => {
+                        let left = dfs(&tree, row, col - 1, &mut memo);
+                        let right = dfs(&tree, row, col + 1, &mut memo);
+                        left + right
+                    }
+                    '.' => dfs(&tree, row + 1, col, &mut memo),
+                    _ => panic!("Unexpected input: {}", tree[row][col] as char),
+                };
+                memo.insert((row, col), count);
+                count
+            }
+        }
+    } else {
+        1
+    }
 }
